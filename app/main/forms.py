a56bd586827ef52d73d8cf8bf242f021289bcbba
ll <2226549571@qq.com>
2016-10-25 22:09:30 +0800

    alpha .1

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 
 class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_USERNAME = '2226549571'
+    MAIL_PASSWORD = 'mvkcrvyqfnnjeceb'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[HIS]'
+    FLASKY_MAIL_SENDER = '2226549571@qq.com'
+    FLASKY_ADMIN = '328957392@qq.com'
 
     @staticmethod
     def init_app(app):
